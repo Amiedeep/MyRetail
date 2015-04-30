e6752fb6151a3fb85e04ebf757988463fdf02cc6
ajitsingh <jeetsingh.ajit@gmail.com>
2015-04-30 13:08:04 +0530

    show my cart button on product details page

--- app/src/main/java/com/myretail/myretail/activities/ProductDetailActivity.java
+++ app/src/main/java/com/myretail/myretail/activities/ProductDetailActivity.java
@@ -1,6 +1,8 @@
 package com.myretail.myretail.activities;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -41,6 +43,17 @@ public class ProductDetailActivity extends Activity {
                 dataBaseHelper.addItemToCart(item.getId());
                 Toast.makeText(getApplicationContext(), "Item is added to cart Successfully", Toast.LENGTH_SHORT).show();
                 DatabaseManager.backupDB();
+            }
+        });
+
+        Button myCartButton = (Button) findViewById(R.id.my_cart_button);
+
+        myCartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                Intent intent = new Intent(context, MyCartActivity.class);
+                startActivity(intent);
             }
         });
     }
